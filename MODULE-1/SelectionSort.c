0ef3bb5861085c55f4db107d243244fcb48e5407
Sanjeev varma <94509003+sanjeev07-debug@users.noreply.github.com>
2022-09-27 23:29:06 +0530

    Update SelectionSort.c

--- MODULE-1/SelectionSort.c
+++ MODULE-1/SelectionSort.c
@@ -1,70 +1,35 @@
-// Algorithm: step 1: select the first element of the list.
-//            step 2: compare the next element (i+1).
-//            step 3: if the next element is smaller thent current element the swap.
-//            step 4: repeate the steps untill you get the array sorted.
-
-
+//algorithm  : now for selection sort the smaller element will go to the low index position
 #include<stdio.h>
 
-void selectsort(int arr[],int n) {
-        int i,j;
-
-        for(i = 0; i<n-1; i++) {
-                for(j = i+1;j<n;j++) {
-                        if(arr[i]>arr[j]) {
-                                int temp = arr[i];
-                                arr[i] = arr[j];
-                                arr[j] = temp;
-                        }
-                }
+void select(int arr[],int n) {
+    int i,j;
+    for(i = 0;i<n;i++) {
+        for(j =i+1;j<n;j++) {
+            if(arr[i]>arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+    }
 }
 
-void printarray(int arr[],int n) {
-        int i;
-        for(i = 0;i<n;i++) {
-                printf("%d",arr[i]);
-        printf("\n");
+//to print the array
+
+void printarray(int arr[],int size) {
+    int i;
+    for(i = 0;i<size;i++) {
+        printf("%d",arr[i]);
+    printf("\n");
+    }
 }
-
-
 int main() {
-        int arr[] = {75,23,45,78,98,54};
-        int n = sizeof(arr)/sizeof(arr[0]);
-        selectionsort(arr,n);
-        printarray(arr, n);
-        return 0;
-}
-~        
+    int arr[6] = {3,2,4,1,9,5};
+    int N = sizeof(arr) /sizeof(arr[0]);
+    select(arr,N);
+    //for printing the array
 
-#include<stdio.h>
-
-void selectsort(int arr[],int n) {
-        int i,j;
-
-        for(i = 0; i<n-1; i++) {
-                for(j = i+1;j<n;j++) {
-                        if(arr[i]>arr[j]) {
-                                int temp = arr[i];
-                                arr[i] = arr[j];
-                                arr[j] = temp;
-                        }
-                }
-        }
+    printarray(arr,N);
+    return 0;
 }
 
-void printarray(int arr[],int n) {
-        int i;
-        for(i = 0;i<n;i++) {
-                printf("%d",arr[i]);
-        printf("\n");
-}
-
-
-int main() {
-        int arr[] = {75,23,45,78,98,54};
-        int n = sizeof(arr)/sizeof(arr[0]);
-        selectsort(arr,n);
-        printarray(arr, n);
-        return 0;
-}
