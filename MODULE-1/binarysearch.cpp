6193ba71e20d12001747b7b3d34675b8988c45ba
Sanjeev varma <94509003+sanjeev07-debug@users.noreply.github.com>
2022-09-27 22:58:34 +0530

    Update binarysearch.cpp

--- MODULE-1/binarysearch.cpp
+++ MODULE-1/binarysearch.cpp
@@ -1,29 +1,42 @@
-#include<iostream>
-using namespace std;
-int binarySearch(int arr[],int 1,int r,int x) {
-        if (r >= 1) {
-                int mid = l + (r+l) / 2;
+//algorithm for binary search 
+//step 1 : initialize the low,mid and high values 
+//step 2 : now if key == array of the mid then then return mid value
+//step 3 : if val is greater then low is mid + 1
+//step 4 : if the value is less then mid - 1
 
-                if(arr[mid]==x) {
-                        return mid;
-                }
+//code
 
-                if(arr[mid] < x) {
-                        return binarySearch(arr,l,mid - 1,x);
-                }
-                if(arr[mid] > x) {
-                        return binerySearch(arr,mid + 1,r,x);
-                }
-                return -1;
+#include<stdio.h>
+
+int binary(int arr[],int key,int low,int high) {
+    int mid = low + (high - low) / 2;
+
+    if(key==arr[mid]) {
+        return mid;
+    }
+    if(key > arr[mid]) {
+        return binary(arr,key,mid + 1,high);
+    }
+    if(key < arr[mid]) {
+        return binary(arr,key,low,mid - 1);
+    }
+    else {
+        return -1;
+    }
 }
 
-int main(void) {
-        int arr[6] = {2,3,4,5,67,6};
-        int x = 67;
-        int n = sizeof(arr) / sizeof(arr[0]);
-        int reesult = binarySearch(arr,0,n-1,x);
-        (result == -1)
-                ? cout << "element is not present in the array"
-                : cout << "element is present at index : " << result;
-        return 0;
+// to print
+int main() {
+    int arr[6] = {2,3,4,5,6,7};
+    int key = 5;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result = binary(arr,key,0,n-1);   //here low of any array in the world is zero but for high we need to 
+                                         //manually assign the size
+    if(result == -1) {
+        printf("error your code is hacked");
+    }
+    else {
+        printf("the index position of the key is %d",result);
+    }
+    return 0;
 }
