8531d4d5f9ee9d9b9c5e0a39cb808e13c074aa21
Sanjeev varma <94509003+sanjeev07-debug@users.noreply.github.com>
2022-10-14 23:04:10 +0530

    Create insertbegin.cpp

---
+++ MODULE-2/C++/insertbegin.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+struct node {
+  int data;
+  struct node * next;
+};
+void begin(int value) {
+    newnode = (struct node * )malloc(sizeof(struct node));
+    newnode -> data = value;
+
+    if(head == NULL) {
+      newnode -> next = NULL;
+      head = newnode;
+    }
+
+    else {
+      newnode -> next = head;
+      head = newnode;
+    }
+//    newnode -> next = head;
+//    head = newnode;
+
+}
+void display(struct node * head) {
+    struct node * pointer = head;
+
+    while(pointer != NULL) {
+    cout << pointer -> data;
+    pointer = pointer -> next;
+
+    }
+    cout << "null" << endl;
+}
+
+int main() {
+struct node *head;
+struct node *second;
+struct node *third;
+
+
+
+
+  head = (struct node *)malloc(sizeof(struct node));
+  second = (struct node *)malloc(sizeof(struct node));
+  third = (struct node *)malloc(sizeof(struct node));
+
+  head -> data = 56;
+  head -> next = second;
+
+  second -> data = 232;
+  second -> next = third;
+
+  third -> data = 435;
+  third -> next = NULL;
+
+  begin(52);
+  display(head);
+
+  return 0;
+}
