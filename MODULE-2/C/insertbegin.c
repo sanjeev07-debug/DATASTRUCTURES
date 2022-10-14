129640e9a427f4cd6e88b5ec0f13ea8560cfa895
Sanjeev varma <94509003+sanjeev07-debug@users.noreply.github.com>
2022-10-14 23:02:53 +0530

    Update insertbegin.c

--- MODULE-2/C/insertbegin.c
+++ MODULE-2/C/insertbegin.c
@@ -1,43 +1,62 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+using namespace std;
 struct node {
-    int data;
-    struct node * next;
+  int data;
+  struct node * next;
 };
+void begin(int value) {
+    newnode = (struct node * )malloc(sizeof(struct node));
+    newnode -> data = value;
 
-void begin(struct node * head,int * newnode)
-{
-    newnode = malloc(sizeof(struct node));
-    printf("enter the data u want to insert : ");
-    scanf("%d",&newnode -> data);
-    newnode -> next = head;
-    head = newnode;
-    return head;
-
-}
-
-void display(struct node * pointer)
-{
-    while(pointer != NULL) {
-        printf("element : : %d",pointer -> data);
-        pointer = poniter -> next;
+    if(head == NULL) {
+      newnode -> next = NULL;
+      head = newnode;
     }
 
+    else {
+      newnode -> next = head;
+      head = newnode;
+    }
+//    newnode -> next = head;
+//    head = newnode;
+
+}
+void display(struct node * head) {
+    struct node * pointer = head;
+
+    while(pointer != NULL) {
+    printf("%d",pointer -> data);
+    pointer = pointer -> next;
+
+    }
+    printf("NUll\n");
 }
 
-int main()
-{
-    struct node * head;
-    struct node * second;
-    struct node * third;
+int main() {
+struct node *head;
+struct node *second;
+struct node *third;
 
-    head = (struct node *)malloc(sizeof(struct node));
-    second = (struct node *)malloc(sizeof(struct node));
-    third = (struct node *)malloc(sizeof(struct node));
 
-    begin(head,&543);
-    display(head);
 
+
+  head = (struct node *)malloc(sizeof(struct node));
+  second = (struct node *)malloc(sizeof(struct node));
+  third = (struct node *)malloc(sizeof(struct node));
+
+  head -> data = 56;
+  head -> next = second;
+
+  second -> data = 232;
+  second -> next = third;
+
+  third -> data = 435;
+  third -> next = NULL;
+
+  begin(52);
+  display(head);
+
+  return 0;
 }
